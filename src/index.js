b0a96c1dce55648218ce554bed295bc53c132e6d
sashman <sashman90@gmail.com>
2018-12-19 23:49:23 +0000

    Adding firebase

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import indexRoutes from "routes/index.jsx";
 const hist = createBrowserHistory();
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000"
+  uri: process.env.REACT_APP_SECRET_CODE || "http://localhost:4000"
 });
 
 ReactDOM.render(
